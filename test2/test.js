1db8b97064480b9d628b1c566cb428c00cf6f06b
Patrik Forsberg <patfor@gmail.com>
2020-01-05 11:11:38 +0100

    Initial Commit

---
+++ test2/test.js
@@ -0,0 +1,14 @@
+/**
+ * Copyright (c) Patrik Forsberg <patrik.forsberg@coldmind.com> - All Rights Reserved
+ * Unauthorized copying of this file, via any medium is strictly prohibited
+ * Proprietary and confidential
+ * January 2020
+ */
+var chokidar = require('chokidar');
+const watchDir = "/Users/patrikforsberg/Projects/Wizum/wizum.node.backend.git";
+var watcher = chokidar.watch(watchDir, { ignored: /^\./, persistent: true });
+watcher
+    .on('add', function (path) { console.log('File', path, 'has been added'); })
+    .on('change', function (path) { console.log('File', path, 'has been changed'); })
+    .on('unlink', function (path) { console.log('File', path, 'has been removed'); })
+    .on('error', function (error) { console.error('Error happened', error); });
